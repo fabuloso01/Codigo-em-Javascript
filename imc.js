a75533dd9e48aea967f64eb7e9c29ebc989b5be2
fabuloso01 <fabuloso01@github.com>
2022-07-30 15:53:19 -0400

    commit

--- imc.js
+++ imc.js
@@ -1,3 +1,4 @@
+/*
 <script>
 
 	let peso = 63.5;
@@ -18,3 +19,33 @@
 	}
 
 </script>
+*/
+
+let metodo = "Dinheiro"
+
+
+switch(metodo) {
+    case "Dinheiro":
+        alert("Pagamento em dinheiro")
+        break;
+    case "Cartão de débito":
+        alert("Pagamento em cartão de débito")
+        break;
+    case "Cartão de crédito":
+        alert("Pagamento em cartão de crédito")
+        break;
+    case "Cheque": 
+        alert("Pagamento em cheque")
+        break;
+    case "Carnê":
+        alert("Pagamento por carnê")
+        break;
+    case "Vale alimentação":
+        alert("Pagamento por vale alimentação")
+        break;
+    default:
+        alert("Opção de pagamento inválida!")
+        break;
+     
+        
+}
